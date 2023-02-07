b116c04ef2fe6a7ab943cfa733b063a3e88b04c3
thiagomnzoliveira <thiagomodev@gmail.com>
2023-02-07 18:41:50 -0300

    fix numbers on display

--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -3,33 +3,38 @@ import './Calculator.css'
 import { useState } from 'react'
 
 const Calculator = () => {
-    const [displayNumber, setDisplayNumber] = useState('')
-    console.log(displayNumber)
+    const [displayNumber, setDisplayNumber] = useState([])
+    /* console.log(displayNumber) */
+
+    const handleExpression = (exp) => {
+        console.log(exp)
+    }
 
     return (
         <div className='calculator-container'>
             <div className='display'>
-                <input type='text' onChange={(e) => setDisplayNumber(e.target.value)} />
+                <p>{displayNumber}</p>
             </div>
             <div className='buttons'>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>X</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>÷</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>-</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 8])}>8</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 7])}>7</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 9])}>9</button>
+                <button >X</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 4])}>4</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 5])}>5</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 6])}>6</button>
+                <button >÷</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 1])}>1</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 2])}>2</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 3])}>3</button>
+                <button >-</button>
                 <button>DEL</button>
-                <button>0</button>
-                <button>,</button>
-                <button>+</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, 0])}>0</button>
+                <button onClick={() => setDisplayNumber(arr => [...arr, ','])}>,</button>
+                <button >+</button>
                 <button>AC</button>
-                <button>=</button>
+                <button onClick={() => handleExpression(displayNumber
+                )}>=</button>
 
             </div>
         </div>
